b8eec8fe97978ad7645676c395c92981025eb8bb
jmt76 <jmt7611@outlook.com>
2024-10-22 17:58:36 -0400

    more questions and points system

--- python_1/name=input("hello user! what is your nic.py
+++ python_1/name=input("hello user! what is your nic.py
@@ -1,6 +1,6 @@
 name=input("hello user! what is your nickname?")
 
-print("Hello,", name,", welcome to my quiz. you need 1500 points to win, each correct answer grants you 150 and every wrong answer takes away 50, some questions will grant you extra points (this is very unserious)")
+print("Hello,", name,", welcome to my quiz. you need 1500 points to win, each correct anser grants you 150 and every wrong answer takes away 50 (this is extremely unserious)")
 
 # start of quiz
 
@@ -28,9 +28,9 @@ else:
     points -= 50
 
 # Question 3
-print("Spell red. (hint: beetlejuice)")
+print("Spell red.")
 answer = input("Write your answer: ")
-if answer == 'lster' or 'LSTER':
+if answer == 'lster':
     print("Correct! Bonus 100 +250 pts")
     points += 250
 else: 
@@ -40,7 +40,7 @@ else:
 # Question 4
 print("Who you gonna call?")
 answer = input("Write your answer: ")
-if answer.lower() == 'ghostbusters' or 'Ghostbusters':  # Case insensitive
+if answer.lower() == 'ghostbusters':  # Case insensitive
     print("Correct! Bonus 50 +200 pts")
     points += 200
 else: 
@@ -60,7 +60,7 @@ else:
 # Question 6
 print("I am _____?")
 answer = input("Write your answer: ")
-if answer.lower() == 'steve' or 'Steve':  # Case insensitive
+if answer.lower() == 'steve':  # Case insensitive
     print("Correct! Bonus 100 +250 pts")
     points += 250
 else: 
@@ -77,52 +77,5 @@ else:
     print("Wrong! -50 pts")
     points -= 50
 
-print("9 x 9")
-answer = input("Write your answer: ")
-if answer == '81':
-    print("Correct! +150 pts")
-    points += 150
-else: 
-    print("Wrong! -50 pts")
-    points -= 50
-
-print("What was the former capitolo of Japan?")
-answer = input("Write your answer: ")
-if answer == 'kyoto' or 'Kyoto':
-    print("Correct! +150 pts")
-    points += 150
-else: 
-    print("Wrong! -50 pts")
-    points -= 50
-
-print("Does Kay have superpowers?")
-answer = input("Write your answer: ")
-if answer == 'true' or 'yes':
-    print("Correct! bonus 100 +250 pts")
-    points += 250
-else: 
-    print("Wrong! -50 pts")
-    points -= 50
-
-print("slang for 'Not genuine or true; false'")
-answer = input("Write your answer: ")
-if answer == 'bogus' or 'Bogus':
-    print("Correct! bonus 100 +250 pts")
-    points += 250
-else: 
-    print("Wrong! -50 pts")
-    points -= 50
-
 # Final score
 print("Total points:", points)
-''' 
-if points > 10:
-    print()
-else:
-    
-
-'''
-
-
-
-
